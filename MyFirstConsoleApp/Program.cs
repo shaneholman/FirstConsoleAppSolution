c95cd83a97157890447ba98fcb3805117b1434a7
Shane Holman <Sholman@tql.com>
2024-06-11 13:37:30 -0400

    added file for first console app

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MyFirstConsoleApp;
+
+    internal class Program {
+        public static string Name { get; set; } = "Shane"; //property 
+        static void Main(string[] args) { // method
+        
+            System.Console.WriteLine($"Hello, {Name} World!");
+       
+        }
+    }
+ 
